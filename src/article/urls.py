9bd2b8f7822bd088802a9f7519d092961521e3a8
kyoya <m.kyoya777@gmail.com>
2021-03-07 09:38:47 +0900

    viewsをviews_login views_userに分割

--- src/article/urls.py
+++ src/article/urls.py
@@ -2,32 +2,52 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.urls import path
 
-from . import views
+import article.views.views_login
+import article.views.views_user
+
+from .views import views_login
+from .views import views_user
 
 app_name = "article"
 urlpatterns = [
-    path("index/", views.IndexView.as_view(), name="index"),
-    path("<int:article_id>/detail/", views.DetailView.as_view(), name="detail"),
-    path("prepare/", views.PrepareArticle.as_view(), name="prepare_post"),
-    path("post/", views.PostArticle.as_view(), name="post"),
-    path("login/", views.Login.as_view(), name="login"),
-    path("logout/", views.Logout.as_view(), name="logout"),
-    path("login/index/", views.LoginIndex.as_view(), name="login_index"),
+    path("index/", views_user.IndexView.as_view(), name="index"),
+    path(
+        "<int:article_id>/detail/",
+        views_user.DetailView.as_view(),
+        name="detail",
+    ),
+    path(
+        "prepare/",
+        views_login.PrepareArticle.as_view(),
+        name="prepare_post",
+    ),
+    path("post/", views_login.PostArticle.as_view(), name="post"),
+    path("login/", views_login.Login.as_view(), name="login"),
+    path("logout/", views_login.Logout.as_view(), name="logout"),
+    path(
+        "login/index/",
+        views_login.LoginIndex.as_view(),
+        name="login_index",
+    ),
     path(
         "login/<int:article_id>/detail/",
-        views.LoginDetail.as_view(),
+        views_login.LoginDetail.as_view(),
         name="login_detail",
     ),
     path(
         "login/<int:article_id>/edit/",
-        views.LoginEdit.as_view(),
+        views_login.LoginEdit.as_view(),
         name="login_edit",
     ),
     path(
         "login/<int:pk>/delete/",
-        views.LoginDeleteArticle.as_view(),
+        views_login.LoginDeleteArticle.as_view(),
         name="login_delete",
     ),
-    path("login/add-article/", views.AddCategory.as_view(), name="add_category"),
+    path(
+        "login/add-article/",
+        views_login.AddCategory.as_view(),
+        name="add_category",
+    ),
     url(r"mdeditor/", include("mdeditor.urls")),
 ]
